c51231f8450321262db234d6fd91da412ef7c79e
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-11-18 18:27:55 -0300

    Adds DPK01-7

--- engineering-problems/DPK01/DPK01_impl_07.py
+++ engineering-problems/DPK01/DPK01_impl_07.py
@@ -14,20 +14,17 @@ position = 0
 def get_letter(arg_word, arg_position):
         
     if arg_position <= word_len:
-        #TODO - It's working, now I need to revert the word, it's printing in the same order
-        print("before incrementing",arg_position)
-        print(arg_word[arg_position])
+        reverted_position = word_len - arg_position
         
         
-        #Position is now incremented
         arg_position += 1
-        print("after incrementing",arg_position)
-    
+
         #Recursion
         get_letter(arg_word, arg_position)
 
 
 
 #Starts in the position 0
-get_letter(word, position)
+coiso = get_letter(word, position)
+print(coiso)
 
