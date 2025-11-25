f85d88cd5af78e1e893cb44b272b3a9ecd66f5e1
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-11-24 22:15:17 -0300

    Adds impl08

--- engineering-problems/DPK01/DPK01_impl_08.py
+++ engineering-problems/DPK01/DPK01_impl_08.py
@@ -1,3 +1,14 @@
 word = "hello"
 word_len = len(word)
+word_list = []
+drow = ""
 
+for letter in word:
+    word_list.append(letter)
+
+
+for i in range(len(word_list) -1, -1, -1):
+    drow += word_list[i]
+
+
+print(drow)
