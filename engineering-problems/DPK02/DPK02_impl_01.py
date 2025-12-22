4cbeda1e538d8ad1718b3363f1e445dcf189a0ba
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-12-22 12:12:08 -0300

    DPK02: Finishes first implementation

--- engineering-problems/DPK02/DPK02_impl_01.py
+++ engineering-problems/DPK02/DPK02_impl_01.py
@@ -1,19 +1,11 @@
 def revert_list(list):
     list_size = len(list)
     i = 0
-    reverted_list = list
-    print("list", list)
-    # print("reverted", reverted_list)
-    print("list_size", list_size)
+    reverted_list = [None] * list_size
     
     while i < list_size:
         current_position = list_size - i - 1
-        print("current_position", current_position)
-        print("current_value", list[current_position] )
-        
-        
         reverted_list[i] = list[current_position]
-        print("reverted", reverted_list)
         i += 1
     return reverted_list
 
