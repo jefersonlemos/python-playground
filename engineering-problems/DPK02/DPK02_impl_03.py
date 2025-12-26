2793475ddcfc9f69c07e57d3fc64979fb9fdb009
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-12-25 21:56:30 -0400

    DPK02: Starts the third implementation

---
+++ engineering-problems/DPK02/DPK02_impl_03.py
@@ -0,0 +1,19 @@
+reverted_list = []
+
+def revert_list(list):
+    print(list.index(10))
+
+    
+    # list_size = len(list)
+    # last_list_item = list_size - 1
+    # list_item = last_list_item
+    
+    # reverted_list.append(list[list_item])
+    # reverted_list.index()
+
+    # if list_item == 0:
+    #     list_item -= 1
+    #     return revert_list(list)
+    
+list = [5,6,7,8,9,10,11,12,13,14,15]
+print(revert_list(list))
