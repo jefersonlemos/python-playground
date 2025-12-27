ce4432d51536a7fbb64c8c43abb5e8fd64ac1960
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-12-27 13:56:48 -0400

    DPK02: Third implementation (fourth attempt worked after lunch)

--- engineering-problems/DPK02/DPK02_impl_03.py
+++ engineering-problems/DPK02/DPK02_impl_03.py
@@ -1,24 +1,21 @@
-def revert_list(list,l,r):
-    if r < 0:
-        print("coco")
-        return reverted_list
-    else:
-        reverted_list[l] = 1
-        l += 1
-        r -= 1
-        return l,r
+def organize_list(list,l,r):
+    reverted_list[l] = list[r]
+    return l,r
 
-def thing(list,l,r):
-    revert_list(list,l,r)
+def revert_list(list,l,r):
+    organize_list(list,l,r)
     l += 1
     r -= 1
-    return thing(list,l,r)
+
+    if r < 0:
+        return reverted_list
+    return revert_list(list,l,r)
 
 
-list = [5,6,7,8]
+list = [5,6,7,8,9,10,11,12,13,14,15]
 list_size = len(list)
 reverted_list = [None] * list_size
 l = 0
 r = list_size - 1
 
-print(thing(list,l,r))
+print(revert_list(list,l,r))
