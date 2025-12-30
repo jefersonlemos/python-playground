d463fd3b9c10809ca3921bb47bb9f5f0304b7648
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-12-30 12:25:11 -0400

    DPK02: Starts fifth implementation

--- engineering-problems/DPK02/DPK02_impl_05.py
+++ engineering-problems/DPK02/DPK02_impl_05.py
@@ -1,4 +1,13 @@
 def revert_list(list):
+    s = len(list) - 1
+    r = s 
+    l = 0
+    for item in list:
+        list[l] = list[r]
+        l += 1
+        r -= 1
+    if l == s:
+        return list
 
 
 
