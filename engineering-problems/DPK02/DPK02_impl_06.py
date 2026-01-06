b811467376adbabda9cccbf9f0570890edc87be9
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-06 15:52:06 -0300

    DPK02: Starts sixth implementation

--- engineering-problems/DPK02/DPK02_impl_06.py
+++ engineering-problems/DPK02/DPK02_impl_06.py
@@ -1,4 +1,22 @@
+def revert_list(list):
+    l = 0
+    r = len(list) - 1
 
+    while l < r:
+        '''
+        #TODO - Next solution
+        instead of switching l with r, where l is the extreme left and r is the extreme right
+        it will now move l one by one, for instance:
+        list = 1,2,3,4
+        1 - 2,1,3,4 ; 2,3,1,4 ; 2,3,4,1
+        2 - 3,2,4,1 ; 3,4,2,1 ; 
+        3 - 4,3,2,1
+        4 - r equals l -> end
+        the numbers on the left side will move to the end of the list one-by-one
+        with no extra lists
+        '''
 
-list = [1, 2, 3, 4]
+    return list
+
+list = [1,2,3,4,5,6,7,8,9,10,11,12]
 print(revert_list(list))
