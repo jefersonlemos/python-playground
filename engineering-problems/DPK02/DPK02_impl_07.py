0ff8e52fdba8f6c4a210d39920732c640994c01c
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-08 12:18:18 -0300

    DPK02: Starts seventh implementation

---
+++ engineering-problems/DPK02/DPK02_impl_07.py
@@ -0,0 +1,12 @@
+def revert_list(list):
+    '''
+    #TODO - Next solution
+    '''
+
+    l = 0
+    r = len(list) - 1
+
+    return list
+
+list = [1,2,3,4,5,6,7,8,9,10]
+print(revert_list(list))
