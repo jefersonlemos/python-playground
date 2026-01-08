92ba08a929cac61ff67742966132c7f6276e72b6
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-08 13:54:30 -0300

    DPK02-06: Some adjustments

--- engineering-problems/DPK02/DPK02_impl_06.py
+++ engineering-problems/DPK02/DPK02_impl_06.py
@@ -1,21 +1,22 @@
 def revert_list(list):
+    '''
+    #TODO - Next solution
+    instead of switching l with r, where l is the extreme left and r is the extreme right
+    it will now move l one by one, for instance:
+    list = 1,2,3,4
+    1 - 2,1,3,4 ; 2,3,1,4 ; 2,3,4,1
+    2 - 3,2,4,1 ; 3,4,2,1 ; 
+    3 - 4,3,2,1
+    4 - r equals l -> end
+    the numbers on the left side will move to the end of the list one-by-one
+    with no extra lists
+    '''     
     l = 0
     r = len(list) - 1
     c = 0
     n = 1
     while n <= r:
-        '''
-        #TODO - Next solution
-        instead of switching l with r, where l is the extreme left and r is the extreme right
-        it will now move l one by one, for instance:
-        list = 1,2,3,4
-        1 - 2,1,3,4 ; 2,3,1,4 ; 2,3,4,1
-        2 - 3,2,4,1 ; 3,4,2,1 ; 
-        3 - 4,3,2,1
-        4 - r equals l -> end
-        the numbers on the left side will move to the end of the list one-by-one
-        with no extra lists
-        '''
+
         list[c], list[n] = list[n], list[c]
 
         c += 1
@@ -29,5 +30,5 @@ def revert_list(list):
 
     return list
 
-list = [1,2,3,4,5,6,7,8,9,10]
+list = [1,2,3,4]
 print(revert_list(list))
--- engineering-problems/DPK02/DPK02_impl_07.py
+++ engineering-problems/DPK02/DPK02_impl_07.py
@@ -1,12 +1,27 @@
 def revert_list(list):
     '''
-    #TODO - Next solution
+    #TODO - This is done but looks like the solution 06 but just added the for loop
+    What's the difference ? How to do it without the while ?
     '''
 
     l = 0
-    r = len(list) - 1
+    r = len(list) - 1 
+    c = 0
+    n = 1
 
+    for item in list:
+        while n <= r:
+            list[c], list[n] = list[n], list[c]
+
+            print(list)
+            c += 1
+            n += 1
+
+        c = 0
+        n = 1
+        l += 1
+        r -= 1
     return list
 
-list = [1,2,3,4,5,6,7,8,9,10]
+list = [1,2,3,4]
 print(revert_list(list))
