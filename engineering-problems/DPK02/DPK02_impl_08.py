bbcd8eb1d897874e444988bb944000fb06188ab0
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-12 21:58:23 -0300

    DPK02-08: Finishes 8th implementation

--- engineering-problems/DPK02/DPK02_impl_08.py
+++ engineering-problems/DPK02/DPK02_impl_08.py
@@ -1,8 +1,17 @@
 def revert_list(list):
     '''
-    #TODO - Define the approach
+        This one is like impl_05 and impl_03 but it uses recursion instead of a while loop and has a function within another
     '''
-
+    
+    def swap_items(list, l, r):
+        if l >= r:
+            return
+        
+        list[l], list[r] = list[r], list[l]
+        
+        swap_items(list, l + 1, r - 1)
+    
+    swap_items(list, 0, len(list) - 1)
     return list
 
 list = [1,2,3,4]
