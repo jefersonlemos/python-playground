0d3b595f7c54bb1a7f1fd325de166fddacab61d9
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-13 11:39:25 -0300

    DPK02-09: first attempt

--- engineering-problems/DPK02/DPK02_impl_09.py
+++ engineering-problems/DPK02/DPK02_impl_09.py
@@ -2,6 +2,20 @@ def revert_list(list):
     '''
     impl_09: loop over a list using while, add the values to a temp list and then loop over the temp list starting from the end
     '''
+    list_size = len(list)
+    temp_list = [None] * list_size
+    l = 0
+    i = 0
+
+    while i < list_size:
+        temp_list[l] = list[i]
+        l += 1
+        i += 1
+    
+    while i < list_size:
+        list[i] = temp_list[l]
+        l -= 1
+        i += 1    
 
     return list
 
