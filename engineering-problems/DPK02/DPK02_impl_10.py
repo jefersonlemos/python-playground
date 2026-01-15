a6a5369422fe597538d0f6b48ebb02baec70ea09
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-14 22:44:11 -0300

    DPK02-10: Finishes the implementation

--- engineering-problems/DPK02/DPK02_impl_10.py
+++ engineering-problems/DPK02/DPK02_impl_10.py
@@ -3,6 +3,16 @@ def revert_list(list):
     #TODO - a recursion that gets the next number based on index and add it to the left
     '''
 
+    def recursion(i, reverted_list):
+        if i >= len(list):
+            return reverted_list
+        
+        reverted_list.insert(0, list[i])
+        return recursion(i + 1, reverted_list)
+    
+    return recursion(0, [])
+
+
 
 
 list = [1,2,3,4]
