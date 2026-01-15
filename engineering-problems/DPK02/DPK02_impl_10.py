729589cd8cd2c3a180112180d7e7a52121f88aa5
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-14 22:44:49 -0300

    DPK02-10: Finishes the implementation

--- engineering-problems/DPK02/DPK02_impl_10.py
+++ engineering-problems/DPK02/DPK02_impl_10.py
@@ -1,8 +1,5 @@
 def revert_list(list):
-    '''
-    #TODO - a recursion that gets the next number based on index and add it to the left
-    '''
-
+    #Explanation - A recursion that gets the next number based on index and add it to the left
     def recursion(i, reverted_list):
         if i >= len(list):
             return reverted_list
@@ -11,9 +8,6 @@ def revert_list(list):
         return recursion(i + 1, reverted_list)
     
     return recursion(0, [])
-
-
-
 
 list = [1,2,3,4]
 print(revert_list(list))
