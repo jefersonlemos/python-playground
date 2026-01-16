842a5a890d5edf1e6cd1a3263c0eb7a6cc86ddb5
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-15 22:47:16 -0300

    DPK03-01: Starts the implementation

---
+++ engineering-problems/DPK03/DPK03_impl_01.py
@@ -0,0 +1,15 @@
+'''
+Approach: To be defined
+'''
+
+data_map = [
+    {"id": 1, "name": "Kuberneide Arantes", "email": "kuberneto@aol.com"},
+    {"id": 2, "name": "Javagner da Silva ", "email": "javagner@cafecomvenvanse.com"}
+]
+
+def lookup(search_key):
+
+print(lookup("javagner@cafecomvenvanse.com"))
+print(lookup(2))
+print(lookup("Javagner da Silva"))
+print(lookup("kuberneide@aol.com"))
