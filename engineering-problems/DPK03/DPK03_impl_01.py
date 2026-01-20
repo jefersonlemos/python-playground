7ba6da1015e4e0075fa2d7d8cb5febdcd45eb3b2
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-19 23:19:13 -0300

    DPK03-01: Starts the implementation

--- engineering-problems/DPK03/DPK03_impl_01.py
+++ engineering-problems/DPK03/DPK03_impl_01.py
@@ -4,12 +4,17 @@ Approach: To be defined
 
 data_map = [
     {"id": 1, "name": "Kuberneide Arantes", "email": "kuberneto@aol.com"},
-    {"id": 2, "name": "Javagner da Silva ", "email": "javagner@cafecomvenvanse.com"}
+    {"id": 2, "name": "Javagner da Silva ", "email": "javagner@bol.com"},
+    {"id": 3, "name": "Rustina Souza Python", "email": "rusthon@cafecomvenvanse.com"}
 ]
 
-def lookup(search_key):
+def lookup(key):
 
-print(lookup("javagner@cafecomvenvanse.com"))
-print(lookup(2))
-print(lookup("Javagner da Silva"))
-print(lookup("kuberneide@aol.com"))
+    for item in data_map:
+        if isinstance(key, int) and item["id"] == key:
+            return item["name"]
+        
+    
+    return None
+
+print(lookup(1))
