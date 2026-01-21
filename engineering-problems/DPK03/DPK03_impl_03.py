c0ef5e8eb9a17db42e8ca1519077e88452331f9f
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-21 11:16:15 -0300

    DPK03-03: Finishes 3rd implementation

--- engineering-problems/DPK03/DPK03_impl_03.py
+++ engineering-problems/DPK03/DPK03_impl_03.py
@@ -1,5 +1,5 @@
 '''
-Approach: Define and Write down the approach
+Approach: Lookup the map using recursion
 '''
 
 data_map = [
@@ -7,12 +7,29 @@ data_map = [
     {"id": 2, "name": "Javagner da Silva", "email": "javagner@bol.com"},
     {"id": 3, "name": "Rustina Souza Python", "email": "rusthon@cafecomvenvanse.com"}
 ]
-
-def lookup(key):
-
+i = 0
+def lookup(key, i = 0 ):
+    if i >= len(data_map):
+        return None
+        
+    if data_map[i]["id"] == key:
+        return data_map[i]["name"]
+        
+    if data_map[i]["name"] == key:
+        return data_map[i]["email"]
+                    
+    if data_map[i]["email"] == key:
+        return data_map[i]["id"]
     
-    return
+    
+    return lookup(key, i + 1)
+
 
 print(lookup(1))
+#Returns name
+ 
 print(lookup("Javagner da Silva"))
+#Returns email
+
 print(lookup("rusthon@cafecomvenvanse.com"))
+#Returns id
