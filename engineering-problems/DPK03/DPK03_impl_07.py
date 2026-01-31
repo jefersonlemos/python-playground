287193892a304185ea5cade3a28683cfb2b58677
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-01-30 21:58:15 -0300

    DPK03: Starts the seventh implementation

---
+++ engineering-problems/DPK03/DPK03_impl_07.py
@@ -0,0 +1,23 @@
+'''
+Approach: Define and Write down the approach
+'''
+
+data_map = [
+    {"id": 1, "name": "Kuberneide Arantes", "email": "kuberneto@aol.com"},
+    {"id": 2, "name": "Javagner da Silva", "email": "javagner@bol.com"},
+    {"id": 3, "name": "Rustina Souza Python", "email": "rusthon@cafecomvenvanse.com"}
+]
+
+def lookup(key):
+
+    
+    return
+
+print(lookup(1))
+#Returns name
+ 
+print(lookup("Javagner da Silva"))
+#Returns email
+
+print(lookup("rusthon@cafecomvenvanse.com"))
+#Returns id
