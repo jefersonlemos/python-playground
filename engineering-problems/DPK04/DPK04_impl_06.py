286f83068c0fd9456ee83023046aa9ab93428814
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-03-03 09:23:37 -0300

    DPK04: Add implementation 06

--- engineering-problems/DPK04/DPK04_impl_06.py
+++ engineering-problems/DPK04/DPK04_impl_06.py
@@ -1,3 +1,7 @@
+'''
+this one uses a list of string, it's less efficient than the 
+list of tuples because the string has to be parsed
+''' 
 def get_language(country):
     data = "Usa:English," \
     "Brazil:Portuguese," \
