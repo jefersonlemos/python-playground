258b639abc0d2f2bbb1b05119ffe43c5c3eb0e43
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-03-18 10:34:59 -0300

    DPK05: Starts implementation 01

--- engineering-problems/DPK05/DPK05_impl_01.py
+++ engineering-problems/DPK05/DPK05_impl_01.py
@@ -19,28 +19,46 @@ def get_power(name):
             return power[person]
     return None
 
-
-def get_most_powerful(name1, name2):
-    power1 = get_power(name1)
-    power2 = get_power(name2)
+def get_most_powerful(player1, player2):
+    if player1_power > player2_power:
+        return winner == player1
+    elif player1_power == player2_power:
+        draw = True
+        return draw
+    else: 
+        return winner == player2
     
-    if power1 > power2:
-        return name1
-    elif power2 > power1:
-        return name2
-    else:
-        return "draw"
-
-
-def play(name1, name2):
-    winner = get_most_powerful(name1, name2)
     
-    leaderboard...
+    
+    
+    
+    
+    player1_power = get_power(player1)
+    player2_power = get_power(player2)
+    winner_score = 10
+    draw_score = 5
+    loser_score = -5
+
+    return 
+
+    def update_leaderboard(player1, player2): 
+            
+        for player in leaderboard:
+            if player == winner:
+                leaderboard[player] += winner_score
+
+
+
+
+def play(player1, player2):
+    winner = get_most_powerful(player1, player2)
+    
+    # leaderboard...
     return winner
 
 
 print(play("John", "Paul"))
-print(leaderboard)
+# print(leaderboard)
 
-print(play("John", "Ringo"))
-print(leaderboard)
+# print(play("John", "Ringo"))
+# print(leaderboard)
