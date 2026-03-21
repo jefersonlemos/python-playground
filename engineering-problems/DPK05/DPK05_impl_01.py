ef31414db558f07518b03e31fe44e4f973b7d36e
jefersonlemos <jefersonlemosdossantos@gmail.com>
2026-03-21 00:32:10 -0300

    update DPK05_impl_01.py

--- engineering-problems/DPK05/DPK05_impl_01.py
+++ engineering-problems/DPK05/DPK05_impl_01.py
@@ -12,7 +12,6 @@ leaderboard = {
     "Ringo": 0
 }
 
-
 def get_power(name):
     for person in power:
         if person == name:
@@ -23,30 +22,31 @@ def get_most_powerful(player1, player2):
     winner = None
     loser = None
     draw = True
-    winner_score = 10
-    draw_score = 5
-    loser_score = -5
-    
+
     player1_power = get_power(player1)
     player2_power = get_power(player2)
     
-    if player1_power > player2_power:
-        winner = player1
-    elif player1_power == player2_power:
-        draw = True
-    else: 
-        winner = player2
-        
-    for player in leaderboard:
-        if player == player1 or player == pla
-        if draw: 
-            
-        if player == winner:
-            #add score
-            pass
-        
+    if player1_power != player2_power: 
+        if player1_power > player2_power:
+            winner = player1
+            draw = False
+        else: 
+            winner = player2
+            draw = False
+    
+    if not draw:
+        if winner == player1: 
+            loser = player2
+        else: 
+            loser = player1
 
-    return
+        leaderboard[winner] += 10
+        leaderboard[loser] -= 5
+    else: 
+        leaderboard[player1] += 5
+        leaderboard[player2] += 5
+        
+    return leaderboard 
 
 def play(player1, player2):
     winner = get_most_powerful(player1, player2)
