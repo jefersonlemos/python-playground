006071079a15fe55cdb97b4d12b315eee983b6dc
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-04-01 11:52:25 -0300

    DPK05:  implementation 04 - battle 1

--- engineering-problems/DPK05/DPK05_impl_04.py
+++ engineering-problems/DPK05/DPK05_impl_04.py
@@ -31,7 +31,37 @@ def get_power (player, i):
     return get_power(player, i + 1)
 
 
-def play(player):
-    return get_power(player,0)
+# 2 - battle
+def battle (battle_players):
+    players_list = list(power.keys())
+    players_to_battle = []
 
-print(play("John"))
+    if battle_players == None: 
+        return None
+    
+    if len(battle_players) != 2:
+        print(len(battle_players))
+        return "Num of Players must be 2"
+    
+    i = 0
+    is_match = True
+    while i < len(battle_players):
+        for player in battle_players:
+            for player_from_list in players_list:
+                if player == player_from_list:
+                    i += 1
+                elif i < 1 and player != player_from_list:
+                    return f"{player} not found"
+                    next
+                elif i == 1 and player != player_from_list:
+                    i += 1
+                    return f"{player} not found"
+
+        
+
+
+def play(players):
+    # return get_power(player,0)
+    return battle(battle_players=players)
+
+print(play(players=["John", "Marcio"]))
