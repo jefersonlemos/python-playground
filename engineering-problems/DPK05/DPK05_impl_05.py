fbd431b8db4503cf1ae7fa77f67ba3c9c64cf940
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-04-07 09:21:07 -0300

    DPK05:  implementation 05 - get most powerful 1

--- engineering-problems/DPK05/DPK05_impl_05.py
+++ engineering-problems/DPK05/DPK05_impl_05.py
@@ -16,3 +16,16 @@ leaderboard = {
 def get_power(player):
 	return power.get(player)
 
+
+def get_most_powerful(player1, player2):
+	power1 = get_power(player1)
+	power2 = get_power(player2)
+
+	if power1 is None or power2 is None:
+		return None
+
+	if power1 > power2:
+		return player1
+	if power2 > power1:
+		return player2
+	return "Draw"
