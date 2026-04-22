4d0a5dba88934f9f2fc0f4ec33931ec67db6fc15
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-04-22 12:02:24 -0300

    DPK05:  implementation 07 - battle 1 - defines the battle

--- engineering-problems/DPK05/DPK05_impl_07.py
+++ engineering-problems/DPK05/DPK05_impl_07.py
@@ -10,4 +10,23 @@ def get_power(player):
     return next((power[p] for p in power if p == player), None)
 
 
-print(get_power('John'))
+def get_most_powerful(player1, player2):
+    p1_power = get_power(player1)
+    p2_power = get_power(player2)
+    
+    if p1_power is None or p2_power is None:
+        return None
+    
+    battle_result = (p1_power > p2_power) - (p1_power < p2_power)
+    
+    if battle_result == 1:
+        return player1
+    elif battle_result == -1:
+        return player2
+    else:
+        return None
+
+
+print(get_most_powerful('John', 'Paul'))
+print(get_most_powerful('Paul', 'John'))
+print(get_most_powerful('George', 'Ringo'))
