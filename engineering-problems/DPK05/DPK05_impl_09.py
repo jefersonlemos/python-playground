20ec0653a3d03dff03482f7b3d1e5f3c3fb16e0b
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-05-04 13:38:10 -0300

    DPK05:  implementation 09 - get most powerful

--- engineering-problems/DPK05/DPK05_impl_09.py
+++ engineering-problems/DPK05/DPK05_impl_09.py
@@ -18,6 +18,33 @@ score_rules = {
     "draw": 5,
 }
 
+RESULT_BY_COMPARISON = {
+    1: "player1",
+    0: "draw",
+    -1: "player2",
+}
 
 def get_power(player):
     return power.get(player)
+
+
+def get_most_powerful(player1, player2):
+    player1_power = get_power(player1)
+    player2_power = get_power(player2)
+
+    if player1_power is None or player2_power is None:
+        return None
+
+    comparison = (player1_power > player2_power) - (player1_power < player2_power)
+    result = RESULT_BY_COMPARISON[comparison]
+
+    winners_by_result = {
+        "player1": player1,
+        "player2": player2,
+        "draw": "draw",
+    }
+
+    return winners_by_result[result]
+
+
+print(get_most_powerful("John","Paul"))
