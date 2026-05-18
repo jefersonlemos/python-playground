0873aad2c813b4d9a136b59066e9ccae39cf8678
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-05-18 19:57:37 -0300

    DPK06: impl 03 - tokenizer

--- engineering-problems/DPK06/DPK06_impl_03.py
+++ engineering-problems/DPK06/DPK06_impl_03.py
@@ -12,3 +12,28 @@ def matches_separator(input, separator, index):
         separator_index += 1
 
     return True
+
+def tokenize(input, separator):
+    if separator == "":
+        raise ValueError("separator cannot be empty")
+
+    tokens = []
+    token_start = 0
+    index = 0
+
+    while index < len(input):
+        if matches_separator(input, separator, index):
+            tokens.append(input[token_start:index])
+            index += len(separator)
+            token_start = index
+        else:
+            index += 1
+
+    tokens.append(input[token_start:len(input)])
+    return tokens
+
+
+if __name__ == "__main__":
+    print(tokenize("Hello,World,How,Are,You", ","))
+    print(tokenize("Hello World How Are You", " "))
+    print(tokenize("Hello-World-How-Are-You", "-"))
