e37e62bbc290de5f7c1061a2a319357d13d14cf4
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-05-19 22:20:17 -0300

    DPK06: impl 04 - separator

---
+++ engineering-problems/DPK06/DPK06_impl_04.py
@@ -0,0 +1,15 @@
+def is_separator(input, separator, index):
+    separator_index = 0
+
+    while separator_index < len(separator):
+        input_index = index + separator_index
+
+        if input_index >= len(input):
+            return False
+        if input[input_index] != separator[separator_index]:
+            return False
+
+        separator_index += 1
+
+    return True
+
