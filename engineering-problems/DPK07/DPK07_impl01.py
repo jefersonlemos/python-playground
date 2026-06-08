a23ce6c15a62134cfe051296b1956dd6b35ff2d5
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-06-08 19:17:45 -0300

    DPK07: impl 01 - group by

---
+++ engineering-problems/DPK07/DPK07_impl01.py
@@ -0,0 +1,23 @@
+def group_by(items, group_size):
+    if group_size <= 0:
+        raise ValueError("group_size must be greater than zero")
+
+    grouped_items = []
+    current_group = []
+
+    for item in items:
+        current_group.append(item)
+
+        if len(current_group) == group_size:
+            grouped_items.append(current_group)
+            current_group = []
+
+    if current_group:
+        grouped_items.append(current_group)
+
+    return grouped_items
+
+
+if __name__ == "__main__":
+    print(group_by([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
+    print(group_by(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], 3))
