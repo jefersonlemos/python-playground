9091f71c2ceded7ad82d0d697ff9a9b3b635974c
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2026-06-12 12:35:47 -0300

    DPK07: impl 02 - group by

---
+++ engineering-problems/DPK07/DPK07_impl02.py
@@ -0,0 +1,13 @@
+
+def group(values, size):
+    if values is None or size <= 0 or size is None:
+        raise ValueError("Error with data")
+    
+    grouped_output = []
+
+    # while value in values <= size:
+    #     grouped_output.append[value]
+    return grouped_output
+    
+if __name__ == "__main__":
+    print(group(["Hi","Hello"], 2))
